0dcf26d2664edaa72dcb0525441e12be9a27f4f1
bendn <bend.n@outlook.com>
2024-03-09 22:20:52 +0700

    array + array

--- src/pervasive.rs
+++ src/pervasive.rs
@@ -57,6 +57,36 @@ pub mod array_and_scalar {
     op!(Sub, AASSub, sub);
 }
 
+mod array_and_array {
+    //! traits for array $op scalar
+    macro_rules! op {
+            ($op:ident, $n:ident, $f:ident, $name:ident) => {
+                #[doc = concat!("see [`", stringify!($f), "`](core::ops::", stringify!($op), "::", stringify!($f), ")")]
+                pub trait $n<T, const N: usize> {
+                    #[doc = concat!("apply the [`", stringify!($f), "`](core::ops::", stringify!($op), "::", stringify!($f), ") function to the elements of both of these arrays.")]
+                    fn $name(self, rhs: [T; N]) -> Self;
+                }
+
+                impl<T: core::ops::$op<Output = T> + Copy, const N: usize> $n<T, N> for [T; N] {
+                    fn $name(self, rhs: [T; N]) -> Self {
+                        use crate::ArrayTools;
+                        self.zip(rhs).map(|(a, b)| core::ops::$op::$f(a, b))
+                    }
+                }
+            };
+        }
+    op!(Add, AAAdd, add, aadd);
+    op!(BitAnd, AAAnd, bitand, aand);
+    op!(BitOr, AAOr, bitor, aor);
+    op!(BitXor, AAXor, bitxor, axor);
+    op!(Div, AADiv, div, adiv);
+    op!(Mul, AAMul, mul, amul);
+    op!(Rem, AARem, rem, arem);
+    op!(Shl, AAShl, shl, ashl);
+    op!(Shr, AAShr, shr, ashr);
+    op!(Sub, AASub, sub, asub);
+}
+
 /// see [`not`](core::ops::Not::not)
 pub trait ANot<T, const N: usize> {
     /// apply the [`not`](core::ops::Not::not) function to each element of this array.
@@ -82,7 +112,7 @@ impl<T: core::ops::Neg<Output = T>, const N: usize> ANeg<T, N> for [T; N] {
 /// Prelude for pervasive operations.
 pub mod prelude {
     #[doc(inline)]
-    pub use super::{array_and_scalar::*, scalar_and_array::*, ANeg, ANot};
+    pub use super::{array_and_array::*, array_and_scalar::*, scalar_and_array::*, ANeg, ANot};
 }
 #[test]
 fn x() {
